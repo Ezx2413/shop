9cffe527e617ab1d03789c912c0dae246c6e4347
ezx2413 <ahn2413.ahn@gmail.com>
2022-04-04 00:00:07 +0430

    add category in market

--- routes/web.php
+++ routes/web.php
@@ -30,19 +30,20 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
 
     Route::prefix('market')->namespace('Market')->group(function(){
 
-        //Categories Routes
+        // Categories Routes
+        // Route::resource('/', CategoryController::class);
 
-        // Route::prefix('category')->group(function(){
+        Route::prefix('category')->group(function(){
 
-        //     // Route::get('/',[CategoryController::class , 'index'])->name('admin.market.category.index');
-        //     // Route::get('/create',[CategoryController::class , 'create'])->name('admin.market.category.create');
-        //     // Route::post('/store',[CategoryController::class , 'store'])->name('admin.market.category.store');
-        //     // Route::get('/edit/{id}',[CategoryController::class , 'edit'])->name('admin.market.category.edit');
-        //     // Route::put('/update/{id}',[CategoryController::class , 'update'])->name('admin.market.category.update');
-        //     // Route::delete('/delete/{id}',[CategoryController::class , 'destroy'])->name('admin.market.category.destroy');
+            Route::get('/',[CategoryController::class , 'index'])->name('admin.market.category.index');
+            Route::get('/create',[CategoryController::class , 'create'])->name('admin.market.category.create');
+            Route::post('/store',[CategoryController::class , 'store'])->name('admin.market.category.store');
+            Route::get('/edit/{id}',[CategoryController::class , 'edit'])->name('admin.market.category.edit');
+            Route::put('/update/{id}',[CategoryController::class , 'update'])->name('admin.market.category.update');
+            Route::delete('/delete/{id}',[CategoryController::class , 'destroy'])->name('admin.market.category.destroy');
 
-        // });
+        });
     });
 });
 
-Route::resource('/', CategoryController::class);
+
