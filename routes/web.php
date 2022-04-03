5433538e84ef7a31de697031af9f56891bb62fd1
ezx2413 <ahn2413.ahn@gmail.com>
2022-04-03 23:49:06 +0430

    add category in market

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\Market\CategoryController;
 
 
 /*
@@ -27,4 +28,21 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
 
     Route::get('/',[AdminDashboardController::class , 'index'])->name('admin.index');
 
+    Route::prefix('market')->namespace('Market')->group(function(){
+
+        //Categories Routes
+
+        // Route::prefix('category')->group(function(){
+
+        //     // Route::get('/',[CategoryController::class , 'index'])->name('admin.market.category.index');
+        //     // Route::get('/create',[CategoryController::class , 'create'])->name('admin.market.category.create');
+        //     // Route::post('/store',[CategoryController::class , 'store'])->name('admin.market.category.store');
+        //     // Route::get('/edit/{id}',[CategoryController::class , 'edit'])->name('admin.market.category.edit');
+        //     // Route::put('/update/{id}',[CategoryController::class , 'update'])->name('admin.market.category.update');
+        //     // Route::delete('/delete/{id}',[CategoryController::class , 'destroy'])->name('admin.market.category.destroy');
+
+        // });
+    });
 });
+
+Route::resource('/', CategoryController::class);
